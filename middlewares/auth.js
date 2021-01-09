957320000fa85411bd5ca1d652ec55df0910c35e
Marcos <marcos.venicius@morphy.com.br>
2021-01-09 15:30:58 -0300

    create midlleware auth and controller user

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+
+const jwt = require('jsonwebtoken');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+class Auth {
+
+    checkAuth(req, res, next) {
+        try {
+            const token = req.headers.authorization.split(" ")[1];
+            const decodedToken = jwt.verify(token, process.env.SECRET_JWT_KEY);
+            req.userData = decodedToken;
+            next();
+        } catch (e) {
+            return res.status(401).json({
+                'message': "Invalid or expired token provided!",
+                'error': e
+            });
+        }
+    }
+}
+
+
+module.exports = new Auth();
